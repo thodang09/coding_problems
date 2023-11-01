fccffa090f70dfcf880ada7a0e23e089e8b40a6f
tho <tho@thos-MBP.hsd1.ca.comcast.net>
2023-11-01 16:59:32 -0700

    added star

--- string/star.js
+++ string/star.js
@@ -1,16 +1,33 @@
 
-
-function pattern1(n){
-    let rows = n;
-    console.log(n);
-    for (const item of object) {
-        
-    }
-    for (const item in object) {
-        
-    }
-}
 const pattern2 = (num) => {
+    let rows = num;
+    let str = "";
+    let check = 0;
+
+    for(let i = 0;  i <= rows * 2 -1; i++){
+        if(i<rows){
+            check = 2 * i +1;
+        }
+        else{ 
+            check = 2 * (2 * rows - i) - 3; 
+        } 
+        for(let space = 1; space <= check; space++){
+            str += " ";
+        }
+
+        for(let star = 1; star <= 2*rows-check; star++){
+            str +="* ";
+        }
+
+
+
+
+
+
+        console.log(str);
+        str ="";
+    }
     
 }
 
+pattern2(5);
